fec62aa84876556de4c0022bf7450831f8cae688
Min Thuta Naing <thutaminn118@gmail.com>
2025-02-10 03:22:34 +0700

    admin panel coding and adding menu items

---
+++ admin_panel/src/components/menu-items/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Your Profile</h2>
+      <p>View and edit your profile details here.</p>
+    </div>
+  );
+};
+
+export default Profile;
