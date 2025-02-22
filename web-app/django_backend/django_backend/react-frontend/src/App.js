399481d0aeeba4d2c781c570bc0543eaa01d128f
SoeYuYuKyaw <6531503186@lamduan.mfu.ac.th>
2025-02-22 13:09:43 +0700

    admin

--- web-app/django_backend/django_backend/react-frontend/src/App.js
+++ web-app/django_backend/django_backend/react-frontend/src/App.js
@@ -3,6 +3,7 @@ import SignUp from "./pages/SignupPage";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
 import ResearcherPage from "./pages/ResearcherPage";
+import AdminPage from "./pages/AdminPage";
 // import Login from "./pages/LoginPage";
 // import Welcome from "./pages/WelcomePage";
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/homepage" element={<HomePage/>}/>
         <Route path="/researcher" element={<ResearcherPage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </Router>
   );
