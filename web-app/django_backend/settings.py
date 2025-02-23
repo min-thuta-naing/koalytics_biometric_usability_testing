9f38b1d265e7066345b42ee5309d28941d1ca3b3
Shwin Pyone Thu <6531503183@lamduan.mfu.ac.th>
2025-02-23 14:28:39 +0700

    restframework

--- web-app/django_backend/settings.py
+++ web-app/django_backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_backend"
+    "django_backend",
+    "rest_framework"
 ]
 
 
