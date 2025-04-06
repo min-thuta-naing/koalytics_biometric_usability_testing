6769b706c805814eaf7604d97c9585280602ff05
Min Thuta Naing <thutaminn118@gmail.com>
2025-04-06 21:29:36 +0700

    adding task and countdown in browser in browser

--- web-app/react-frontend/src/pages/Participant/TestCalibration.jsx
+++ web-app/react-frontend/src/pages/Participant/TestCalibration.jsx
@@ -1,54 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
-
-// const TestCalibration = () => {
-//     const { usabilityTestingId } = useParams(); 
-//     const [usabilityTesting, setUsabilityTesting] = useState(null);
-//     const [error, setError] = useState("");
-
-//     // Fetch form details
-//     useEffect(() => {
-//             const fetchUsabilityTestingDetails = async () => {
-//             try {
-//                 const response = await fetch(`http://127.0.0.1:8000/api/usability_testing/${usabilityTestingId}/`);
-//                 if (!response.ok) {
-//                 throw new Error("Failed to fetch usability testing details.");
-//                 }
-//                 const data = await response.json();
-//                 setUsabilityTesting(data);
-//             } catch (err) {
-//                 setError(err.message);
-//             }
-//         };
-//         fetchUsabilityTestingDetails();
-//     }, [usabilityTestingId]);
-
-//     if (error) return <p className="text-red-500">{error}</p>;
-//     if (!usabilityTesting) return <p>Loading...</p>;
-
-//     return (
-//         <div className="bg-[#F0EEED] h-screen"> 
-//             <div className="p-8 border-b border-gray-400">
-//                 <p className="font-funnel font-3xl">{usabilityTesting.id}</p>
-//                 <p className="font-funnel font-3xl">Title: {usabilityTesting.title}</p>
-//                 <p className="font-funnel font-3xl">Task: {usabilityTesting.task}</p>
-//                 <p className="font-funnel font-3xl">Duration: {usabilityTesting.duration}</p>
-//                 <p className="font-funnel font-3xl">Website: <a href={usabilityTesting.website_link} target="_blank" rel="noopener noreferrer">{usabilityTesting.website_link}</a></p>
-//                 {/* "Start Testing" Button */}
-//                 <button 
-//                     onClick={() => window.open(usabilityTesting.website_link, "_blank")}
-//                     className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
-//                 >
-//                     Start Testing
-//                 </button>
-//                 <p className="font-funnel font-3xl">Figma Embed Code: {usabilityTesting.figma_embed_code}</p>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default TestCalibration; 
-
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import React from 'react';
@@ -129,7 +78,7 @@ const TestCalibration = () => {
         mediaRecorder.stop();
       }, 4000); // Stop recording after 4 seconds
   
-      window.open(`/browser-in-browser?url=${encodeURIComponent(usabilityTesting.website_link)}`, "_blank"); // Open website in a new tab
+      window.open(`/browser-in-browser?url=${encodeURIComponent(usabilityTesting.website_link)}&id=${usabilityTestingId}`, "_blank"); // Open website in a new tab
     } catch (error) {
       console.error("Screen recording error:", error);
     }
